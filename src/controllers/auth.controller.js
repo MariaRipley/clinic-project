bb7832b57bdf6ce736d4b70f6c4b72155d8c36d2
MariaRipley <roblesb.maria@gmail.com>
2024-03-05 18:30:54 +0100

    Backend setup: MongoDB, user register, authentication process, generate and save token

---
+++ src/controllers/auth.controller.js
@@ -0,0 +1,36 @@
+//Funciones de registro y login
+
+import User from "../models/user.model.js";
+import bcrypt from "bcryptjs";
+import { createAccessToken } from "../libs/jwt.js";
+
+export const register = async (req, res) => {
+  const { email, password, username } = req.body;
+
+  try {
+    //Encriptar contraseña con bcrypt
+    const passwordHash = await bcrypt.hash(password, 10);
+
+    const newUser = new User({
+      username,
+      email,
+      password: passwordHash,
+    });
+
+    const userSaved = await newUser.save();
+    const token = await createAccessToken({ id: userSaved._id });
+
+    res.cookie("token", token);
+    res.json({
+      id: userSaved._id,
+      username: userSaved.username,
+      email: userSaved.email,
+      createdAt: userSaved.createdAt,
+      updatedAt: userSaved.updatedAt,
+    });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+export const login = (req, res) => res.send("login");
---
+++ src/models/user.model.js
@@ -0,0 +1,29 @@
+//Esquema de la base de datos
+
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      trim: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+//Poder interactuar con los métodos de la base de datos
+export default mongoose.model("User", userSchema);
