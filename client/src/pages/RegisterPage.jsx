5a230496e9c10631865b31433acd1ace37eb8e40
MariaRipley <roblesb.maria@gmail.com>
2024-03-07 19:46:27 +0100

    Add styles to register page

--- client/src/pages/RegisterPage.jsx
+++ client/src/pages/RegisterPage.jsx
@@ -21,42 +21,47 @@ function RegisterPage() {
   });
 
   return (
-    <div className="bg-zinc-800 max-w-md p-10 rounded-md">
-      {registerErrors.map((error, i) => (
-        <div key={i} className="bg-red-500 p-2 text-white">
-          {error}
-        </div>
-      ))}
-      <form onSubmit={onSubmit}>
-        <input
-          type="text"
-          {...register("username", { required: true })}
-          className="w-full bg-zinc-700 text-white px-4 py-2 rounded-md my-2"
-          placeholder="Nombre de usuario"
-        />
-        {errors.username && (
-          <p className="text-red-500">Username is required</p>
-        )}
-        <input
-          type="email"
-          {...register("email", { required: true })}
-          className="w-full bg-zinc-700 text-white px-4 py-2 rounded-md my-2"
-          placeholder="Email"
-        />
-        {errors.email && <p className="text-red-500">Email is required</p>}
-        <input
-          type="password"
-          {...register("password", { required: true })}
-          className="w-full bg-zinc-700 text-white px-4 py-2 rounded-md my-2"
-          placeholder="Password"
-        />
-        {errors.password && (
-          <p className="text-red-500">Password is required</p>
-        )}
-        <button type="submit" className="text-white">
-          Register
-        </button>
-      </form>
+    <div className="bg-cover bg-center h-screen bg-[url('/main-bg.jpg')] flex justify-center items-center">
+      <div className="w-3/4 sm:w-2/3 md:w-1/2 lg:w-1/3 xl:w-1/3 h-1/2 sm:h-1/2 md:h-1/2 lg:h-1/2 xl:h-1/2 bg-gradient-to-b from-sky-200 to-sky-800 p-4 rounded-lg flex justify-center items-center">
+        <form onSubmit={onSubmit}>
+          {registerErrors.map((error, i) => (
+            <div key={i} className="bg-red-500 p-2 text-white">
+              {error}
+            </div>
+          ))}
+          <input
+            type="text"
+            {...register("username", { required: true })}
+            className="w-full bg-sky-100 text-white px-4 py-2 rounded-md my-2"
+            placeholder="Nombre de usuario"
+          />
+          {errors.username && (
+            <p className="text-red-500">Username is required</p>
+          )}
+          <input
+            type="email"
+            {...register("email", { required: true })}
+            className="w-full bg-sky-100 text-white px-4 py-2 rounded-md my-2"
+            placeholder="Email"
+          />
+          {errors.email && <p className="text-red-500">Email is required</p>}
+          <input
+            type="password"
+            {...register("password", { required: true })}
+            className="w-full bg-sky-100 text-white px-4 py-2 rounded-md my-2"
+            placeholder="Password"
+          />
+          {errors.password && (
+            <p className="text-red-500">Password is required</p>
+          )}
+          <button
+            type="submit"
+            className="text-sky-50 bg-sky-950 p-1 rounded-md my-2"
+          >
+            Register
+          </button>
+        </form>
+      </div>
     </div>
   );
 }
